a96524dd743bbb4803e5ef4e0d277bebb4585d13
scarlettiron <scottscarlett@gmail.com>
2022-05-20 22:38:00 -0400

    created booking endpoint

--- booking/views.py
+++ booking/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics, mixins
 from rest_framework.response import Response
 
 from .models import calendar, appointment, classSessionId
-from .serializers import calendar_serializer, classSessionId_serializer
+from .serializers import calendar_serializer, classSessionId_serializer, appointment_serializer
 
 class calendar(generics.RetrieveAPIView):
     queryset = calendar.objects.all()
@@ -16,15 +16,15 @@ class calendar(generics.RetrieveAPIView):
 
 
 class class_session_detail(generics.RetrieveUpdateAPIView):
-    queryset = classSessionId.objects.filter().select_related('classPackage', 'classPackage__user')[0]
+    queryset = classSessionId.objects.filter().select_related('classPackage', 'classPackage__user')
     lookup_field = 'pk'
     serializer_class = classSessionId_serializer
-    
-    def get_queryset(self):
-        pk = self.kwargs['pk']
-        return classSessionId.objects.filter(pk=pk).select_related('classPackage', 'classPackage__user')[0]
 
-    def get(self, request, *args, **kwargs):
-        qs = super().get_queryset()
-        serializer = classSessionId_serializer(qs).data
-        return Response(serializer, status=200)
+
+class appointment_detail(generics.GenericAPIView):
+    queryset = appointment.objects.filter().select_related('packageSessionId')
+    lookup_field = 'user'
+    serializer_class = appointment_serializer
+    
+    def put(self, request, *args, **kwargs):
+        pass
