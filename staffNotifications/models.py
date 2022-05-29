b914f851c7f4bc747eb80ea4954add4a182f7b5b
scarlettiron <scottscarlett@gmail.com>
2022-05-28 22:34:45 -0400

    added signals.py file in verification, created signals to update staffTicket status, update is_instructor on user model to true if status = passed. Boiler plate to create user notification

--- staffNotifications/models.py
+++ staffNotifications/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 
-# Create your models here.
+
+
+staffNotificationReasons = [
+    ('verification', 'Verification'),
+]
+
+flags = [
+    ('green', 'Green'),   
+         ]
+
+status_choices = [
+    ('open', 'Open'),
+    ('closed', 'Closed')
+]
+
+class staff_notification(models.Model):
+    type = models.CharField(choices = staffNotificationReasons, max_length=50)
+    flag = models.CharField(choices = flags, max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=3000, blank=True, null=True)
+    status = models.CharField(choices = status_choices, max_length=20, default='open')
--- verification/models.py
+++ verification/models.py
@@ -4,10 +4,16 @@ User = settings.AUTH_USER_MODEL
 
 class verificationStatus(models.Model):
     status = models.CharField(max_length=50)
+    
+status_options = [
+    ('pending', 'Pending'),
+    ('declined', 'Declined'),
+    ('passed', 'Passed')
+]
 
 class verification(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photoId = models.ImageField(upload_to = 'verifications/photoId/')
     certificate = models.ImageField(upload_to = 'verifications/certificates/')
-    status = models.ForeignKey(verificationStatus, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(choices = status_options, max_length=30, default = 'pending')
     reason = models.CharField(max_length=250)
