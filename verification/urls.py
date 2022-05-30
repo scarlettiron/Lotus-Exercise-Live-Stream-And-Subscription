690974e60e25461c6374ce5e0096e25a224e5171
scarlettiron <scottscarlett@gmail.com>
2022-05-29 20:14:49 -0400

    modified verification view to retreive verififcation table info

--- verification/urls.py
+++ verification/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views as v
 
 urlpatterns = [
-    path('detail/<user>', v.verification_detail.as_view(), name='verification-detail'),
+    path('detail/<int:user>/', v.verification_detail.as_view(), name='verification-detail'),
+
 ]
--- verification/views.py
+++ verification/views.py
@@ -1,27 +1,27 @@
-from rest_framework import generics, mixins, parsers
+from logging import raiseExceptions
+import re
+from rest_framework import generics, mixins, parsers, response
 
 from .models import verification
 from .serializers import verification_serializer
 from .mixins import verification_mixin
 
-class verification_detail(verification_mixin, generics.CreateAPIView, 
+class verification_detail(generics.CreateAPIView, 
                           mixins.RetrieveModelMixin):
     model = verification
+    queryset = verification.objects.get()
     lookup_field = 'user'
     serializer_class = verification_serializer
     parser_classes = [parsers.MultiPartParser, parsers.FormParser, parsers.JSONParser]
     
-    def get_queryset(self, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         user = self.kwargs['user']
         try:
             qs = verification.objects.filter(user = user).order_by('pk')[0]
             print(qs.status)
+            serializer = verification_serializer(qs).data
+            return response.Response(serializer, status=200)
         except:
-            qs = verification.objects.none()
-        return qs
-    
-    
-    
-        
-    
-    
+            return response.Response(status = 404)
+
+
