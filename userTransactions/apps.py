d0ff616a439560e76e7d5a882c2c80dbd3c414ec
scarlettiron <scottscarlett@gmail.com>
2022-06-01 22:09:50 -0400

    imported all signals files into respective app.configs

--- userTransactions/apps.py
+++ userTransactions/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsertransactionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userTransactions'
+    
+    def ready(self):
+        import userTransactions.signals
